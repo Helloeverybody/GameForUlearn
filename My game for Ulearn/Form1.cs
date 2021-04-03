535d991889e1044e20155c34ed228255cba045c0
Bocharov Vsevolod <seva.bocharov@mail.ru>
2021-04-03 20:49:59 +0500

    сборка игры 0.0 - ничего нет

---
+++ My game for Ulearn/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_game_for_Ulearn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        protected override void OnFormClosing(FormClosingEventArgs eventArgs)
+        {
+            var result = MessageBox.Show("Действительно закрыть?", "",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result != DialogResult.Yes)
+                eventArgs.Cancel = true;
+        }
+    }
+}
